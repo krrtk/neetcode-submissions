86a42535aeee3416f2cd1c706d140bf0b4c16c93
krrtk <150647656+krrtk@users.noreply.github.com>
2026-05-29 11:17:59 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++){
+            int tar=target-nums[i];
+            for(int j=0;j<nums.size();j++){
+                if(i!=j && nums[j]==tar){
+                    return {i,j};
+                }
+            }
+        }
+        return {};
+    }
+};

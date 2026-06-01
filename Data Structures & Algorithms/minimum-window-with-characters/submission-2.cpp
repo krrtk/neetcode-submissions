55fa09bc71587fd1a950403d106e97a407e05e34
krrtk <150647656+krrtk@users.noreply.github.com>
2026-06-01 12:39:48 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool chk(map<char, int> &cnt, map<char, int> &mp) {
+        for (auto x : mp) {
+            if (cnt[x.first] < x.second) { 
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string minWindow(string s, string t) {
+        map<char, int> mp;
+        map<char, int> mp1;
+        
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+        }
+        for (int i = 0; i < t.size(); i++) {
+            mp1[t[i]]++;
+            if (mp[t[i]] == 0) {
+                return "";
+            }
+        }
+
+        int i = 0;
+        int j = 0;
+        map<char, int> cnt;
+        int len = INT_MAX;
+        string res = "";
+
+        while (j < s.size() || chk(cnt, mp1)) {
+            bool f = chk(cnt, mp1);
+            
+            if (!f && j < s.size()) {
+                cnt[s[j]]++;
+                j++;
+            } else if (f) {
+                if (len > j - i) {
+                    res = s.substr(i, j - i);
+                    len = j - i;
+                }
+                cnt[s[i]]--;
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        return res;
+    }
+};
